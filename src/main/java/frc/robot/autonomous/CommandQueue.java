b2cd50054c2c0b4f3ddf8ec4998150b587c9a7ea
deshidw <deshidw@gmail.com>
2019-02-18 19:46:49 -0700

    Added Command Documentation

--- src/main/java/frc/robot/autonomous/CommandQueue.java
+++ src/main/java/frc/robot/autonomous/CommandQueue.java
@@ -3,18 +3,28 @@ package frc.robot.autonomous;
 import java.util.List;
 import java.util.ArrayList;
 
+/**
+ * A queue of {@link Command}s that get called every robot update until the command
+ * is complete.
+ */
 public class CommandQueue {
-    
+    /**
+     * A list of {@link Command}s that will dynamically grow and shrink when commands
+     * start and finish.
+     */
     public List<Command> commands = new ArrayList<Command>();
-    
-    public CommandQueue(){
-    // so we dont need anythin in here which is dope
-    }
+
+    /**
+     * Runs each robot update. This method will update {@link Command}s and detect
+     * if a command is finished.
+     */
     public void Periodic() {
         for(Command command : commands) {
-            if(command.IsFinished() == false) {
+            // Update eaach command that is still running.
+            if(command.isFinished == false) {
                 command.Periodic();
             }
+            // Remove the commands that have finished.
             else {
                 command.Finish();
                 commands.remove(command);
--- src/main/java/frc/robot/autonomous/commands/ExampleCommand.java
+++ src/main/java/frc/robot/autonomous/commands/ExampleCommand.java
@@ -2,30 +2,39 @@ package frc.robot.autonomous.commands;
 
 import frc.robot.autonomous.Command;
 
-public class ExampleCommand implements Command {
-    private boolean isFinished = false;
-
+/**
+ * An example for the {@link Command} base class.
+ * <p>
+ * Use this class for reference when creating new {@link Command} extensions.
+ */
+public class ExampleCommand extends Command {
     private int index = 0;
 
+    /*
+    * Command constructor should be used for initializing this at the start of
+    * the commands life-time.
+    */
     public ExampleCommand() {
         System.out.printf("Example command created.");
     }
 
+    /*
+    * Periodic is called every robot update.
+    */
     @Override
     public void Periodic() {
+        // Prints to console 20 times and then ends.
         index++;
         if(index == 20) isFinished = true;
 
         System.out.printf("Example command periodic.");
     }
 
+    /*
+    * Finish is called when the command ends.
+    */
     @Override
     public void Finish() {
         System.out.printf("Example command finished.");
-    }
-
-    @Override
-    public boolean IsFinished() {
-        return isFinished;
     }
 }
